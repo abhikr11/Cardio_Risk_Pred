a4002fd6ab2827563995100013e978396abf9f32
abhikr11 <abhijeet.kr7252@gmail.com>
2024-12-21 17:04:44 +0530

    setup

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setup(
     description="ML model to predict cardiovascular risk",
     url="https://github.com/abhikr11/Cardio_Risk_Pred",
     packages=find_packages(),  # Automatically finds all sub-packages
-    install_requires=get_requirements("requirements.txt"),  # Reads requirements from file
+    install_requires=get_requirements("requirements.txt")  # Reads requirements from file
 )
